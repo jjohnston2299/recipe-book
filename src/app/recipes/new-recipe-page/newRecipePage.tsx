06b07029d9795d46175b0af2c24a678d94bb0798
Jeff Johnston <jjohnston2299@gmail.com>
2025-06-09 11:53:02 -0400

    fix: update imports and components to use new RecipeForm

--- src/app/recipes/new-recipe-page/newRecipePage.tsx
+++ src/app/recipes/new-recipe-page/newRecipePage.tsx
@@ -1,8 +1,10 @@
+'use client';
+
 import RecipeForm from '@/components/RecipeForm/RecipeForm'
 
 export default function NewRecipePage() {
   return (
-    <div className="max-w-4xl mx-auto">
+    <div className="container mx-auto py-8">
       <h1 className="text-3xl font-bold mb-8">Create New Recipe</h1>
       <RecipeForm />
     </div>
